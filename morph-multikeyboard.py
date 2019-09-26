0db5ed66cd00b05f5dc9ddbc559cbb74e48dfa5e
Leo Dawn <leo@leodearden.org>
2019-09-26 17:56:03 +0100

    Begin to collect Morph acces code into class.

--- morph-multikeyboard.py
+++ morph-multikeyboard.py
@@ -3,7 +3,7 @@
 ##########################################################################
 # MIT License
 #
-# Copyright (c) 2013-2017 Sensel, Inc.
+# Copyright (c) 2019 Leo Dawn.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
 # software and associated documentation files (the "Software"), to deal in the Software
@@ -32,64 +32,89 @@ import binascii
 import threading
 
 
-def waitForEnter():
+class SenselError(Exception):
+    def __init__(self, error_num):
+        self.error_num = error_num
+
+
+def warn_on_error(error_num, explaination=''):
+    if error_num != 0:
+        print('Error detected during sensel operation {} ({})'.format(explaination, error_num))
+
+
+class Morph:
+    def __init__(self, index=0):
+        self.handle = self.open(index)
+        self.frame = self.init_frame()
+        # self.info is not currently used, but this is how to get it. Let's take a look and see what info we get
+        self.info = self.close_on_error(sensel.getSensorInfo(self.handle))
+
+    def close_on_error(self, retval):
+        try:
+            error_num, val = retval
+        except:
+            error_num = retval
+            val = None
+        if error_num != 0:
+            print('Error detected. Closing')
+            self.close_sensel()
+            raise SenselError(error_num)
+        return val
+
+    def open(self, index=0):
+        handle = None
+        (error_num, device_list) = sensel.getDeviceList()
+        if device_list.num_devices > index:
+            (error_num, handle) = sensel.openDeviceByID(device_list.devices[index].idx)
+            if error_num != 0:
+                raise SenselError(error_num)
+        return handle
+
+    def init_frame(self):
+        self.close_on_error(sensel.setFrameContent(self.handle, sensel.FRAME_CONTENT_CONTACTS_MASK))
+        frame = self.close_on_error(sensel.allocateFrameData(self.handle))
+        assert frame
+        self.close_on_error(sensel.startScanning(self.handle))
+        return frame
+
+    def scan_frames(self):
+        self.close_on_error(sensel.readSensor(self.handle))
+        (error, num_frames) = sensel.getNumAvailableFrames(self.handle)
+        for i in range(num_frames):
+            self.close_on_error(sensel.getFrame(self.handle, self.frame))
+            print_frame(self.frame)
+
+    def close_sensel(self):
+        if self.handle is not None:
+            warn_on_error(sensel.stopScanning(self.handle), 'stop scanning before close')
+            warn_on_error(sensel.freeFrameData(self.handle, self.frame), 'free frame data before close')
+            warn_on_error(sensel.close(self.handle), 'close handle {}'.format(self.handle))
+
+
+# I don't like this kluge much, but leave it for now, 'til I:
+# TODO: set up an exit handler to replace threaded wait.
+def wait_for_enter():
     global enter_pressed
     input("Press Enter to exit...")
     enter_pressed = True
     return
 
 
-def openSensel(index=0):
-    handle = None
-    (error, device_list) = sensel.getDeviceList()
-    if device_list.num_devices > index:
-        (error, handle) = sensel.openDeviceByID(device_list.devices[index].idx)
-    return handle
-
-
-def initFrame():
-    error = sensel.setFrameContent(handle, sensel.FRAME_CONTENT_CONTACTS_MASK)
-    (error, frame) = sensel.allocateFrameData(handle)
-    error = sensel.startScanning(handle)
-    return frame
-
-
-def scanFrames(frame, info):
-    error = sensel.readSensor(handle)
-    (error, num_frames) = sensel.getNumAvailableFrames(handle)
-    for i in range(num_frames):
-        error = sensel.getFrame(handle, frame)
-        printFrame(frame, info)
-
-
-def printFrame(frame, info):
+def print_frame(frame):
     if frame.n_contacts > 0:
         print("\nNum Contacts: ", frame.n_contacts)
         for n in range(frame.n_contacts):
             c = frame.contacts[n]
             print("Contact ID: ", c.id)
-            if c.state == sensel.CONTACT_START:
-                sensel.setLEDBrightness(handle, c.id, 100)
-            elif c.state == sensel.CONTACT_END:
-                sensel.setLEDBrightness(handle, c.id, 0)
-
-
-def closeSensel(frame):
-    error = sensel.freeFrameData(handle, frame)
-    error = sensel.stopScanning(handle)
-    error = sensel.close(handle)
 
 
 if __name__ == "__main__":
     enter_pressed = False
-    handle = openSensel(1)
-    if handle != None:
-        (error, info) = sensel.getSensorInfo(handle)
-        frame = initFrame()
+    morph = Morph(1)
 
-        t = threading.Thread(target=waitForEnter)
-        t.start()
-        while not enter_pressed:
-            scanFrames(frame, info)
-        closeSensel(frame)
+    t = threading.Thread(target=wait_for_enter)
+    t.start()
+    while not enter_pressed:
+        morph.scan_frames()
+    morph.close_sensel()
 
