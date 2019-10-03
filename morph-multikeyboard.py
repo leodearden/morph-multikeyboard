17c4f53a93795abe2b3255cd33e1e5d76bad8430
Leo Dawn <leo@leodearden.org>
2019-10-03 11:18:29 +0100

    Map touch data onto keyboard events.

--- morph-multikeyboard.py
+++ morph-multikeyboard.py
@@ -30,7 +30,16 @@ sys.path.append('../sensel-api/sensel-lib-wrappers/sensel-lib-python')
 import sensel
 import time
 import traceback
+import xml.etree.ElementTree as ET
+import logging
+import itertools
+import shapely.geometry
+import pyautogui
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+pyautogui.PAUSE = 0.05
 
 class SenselError(Exception):
     def __init__(self, error_num):
@@ -39,7 +48,7 @@ class SenselError(Exception):
 
 def warn_on_error(error_num, explaination=''):
     if error_num != 0:
-        print('Error detected during sensel operation {} ({})'.format(explaination, error_num))
+        logging.error('Error detected during sensel operation {} ({})'.format(explaination, error_num))
 
 
 class Morph:
@@ -56,7 +65,7 @@ class Morph:
             error_num = retval
             val = None
         if error_num != 0:
-            print('Error detected. Closing')
+            logging.error('Error detected. Closing')
             self.close_sensel()
             raise SenselError(error_num)
         return val
@@ -67,7 +76,14 @@ class Morph:
         if error_num != 0:
             raise SenselError(error_num)
         if device_list.num_devices > index:
-            (error_num, handle) = sensel.openDeviceByID(device_list.devices[index].idx)
+            device = device_list.devices[index]
+            self.serial_num = bytearray(device.serial_num).decode()
+            self.com_port = bytearray(device.com_port).decode()
+            logging.debug('Opening sensel device {} (idx: {}, serial_num: {}, com_port: {}'.format(index,
+                                                                                                   device.idx,
+                                                                                                   self.serial_num,
+                                                                                                   self.com_port))
+            (error_num, handle) = sensel.openDeviceByID(device.idx)
             if error_num != 0:
                 raise SenselError(error_num)
         return handle
@@ -85,6 +101,14 @@ class Morph:
             time.sleep(0.05)
         self.close_on_error(sensel.getFrame(self.handle, self.frame))
         return self.frame
+
+    def get_contacts(self):
+        frame = self.get_frame()
+        contacts = {
+            contact.id: contact
+            for contact in frame.contacts
+        }
+        return contacts
 
     def close_sensel(self):
         if self.handle is not None:
@@ -113,14 +137,139 @@ def print_frame(frame):
             print("Contact ID: ", c.id)
 
 
+def gen_gen_simple_key_handler(key_code):
+    def gen_simple_key_hander(polygon: shapely.geometry.Polygon):
+        def key_up():
+            pyautogui.keyUp(key_code)
+
+        def handler(p: shapely.geometry.Point):
+            if polygon.contains(p):
+                pyautogui.keyDown(key_code)
+                return key_up
+            else:
+                return None
+
+        return handler
+
+    return gen_simple_key_hander
+
+
+def gen_gen_modal_key_handler(key_code, on_mode_change):
+    def gen_modal_key_handler(polygon: shapely.geometry.Polygon):
+        simple_handler = gen_gen_simple_key_handler(key_code)(polygon)
+        def modal_key_up(simple_key_up):
+            on_mode_change(False)
+            simple_key_up()
+
+        def modal_handler(p: shapely.geometry.Point):
+            simple_key_up = simple_handler(p)
+            if simple_key_up:
+                on_mode_change(True)
+                return modal_key_up(simple_key_up)
+            else:
+                return None
+
+        return modal_handler
+
+    return gen_modal_key_handler
+
+
+def parse_key_node(key_node):
+    # pull out rectangle coords
+    rect_node = key_node.findall('rect')
+    min_x = rect_node.attrib['x']
+    min_y = rect_node.attrib['y']
+    max_x = min_x + rect_node.attrib['rx']
+    max_y = min_y + rect_node.attrib['ry']
+    rect = shapely.geometry.box(min_x, min_y, max_x, max_y)
+    # pull out label
+    label = ' '.join([chars.lstrip().rstrip() for chars in key_node.itertext()])
+    return rect, label
+
+
+def parse_layout(file_name, keymaps):
+    layout_tree = ET.parse(file_name)
+    # find all groups that have a rect immediate child and text or flowRoot immediate child
+    flowRoot_labelled_key_nodes = layout_tree.getroot().findall('g/rect/../flowRoot/..')
+    text_labelled_key_nodes = layout_tree.getroot().findall('g/rect/../text/..')
+    # TODO: also parse key nodes that have path outlines (instead of rects)
+    layouts = {}
+    # TODO: index layout according to x and y to optimise testing contact points.
+    for mode, keymap in keymaps.items():
+        layout = {}
+        for key_node in itertools.chain(flowRoot_labelled_key_nodes, text_labelled_key_nodes):
+            rect, label = parse_key_node(key_node)
+            handler = keymap[label](rect)
+            layout[label] = handler
+        layouts[mode] = layout
+    return layouts
+
+
+class Keyboard():
+    def __init__(self, morph, layout_file):
+        self.morph = morph
+        self.keymaps = self.generate_keymaps()
+        self.layouts = parse_layout(layout_file, self.keymaps)
+        self.layout = self.layouts['base']
+
+    def generate_keymaps(self):
+        keymap = {}
+        def on_shift_key_change(shift_pressed: bool):
+            if shift_pressed:
+                self.layout = self.layouts['shifted']
+            else:
+                self.layout = self.layouts['base']
+        keymap['Shift'] = gen_gen_modal_key_handler('shift', on_shift_key_change)
+        trivial_keys = [c.upper() for c in 'abcdefghijklmnopqrstuvwxyz']
+        trivial_keys.extend(['F{}'.format(n) for n in range(1, 24)])
+        trivial_keys.extend(
+            ['Esc', 'Tab', 'Caps Lock', 'Alt', 'Ctrl', 'Insert', 'Home', 'End', 'Page Up', 'Page Down', 'Print Screen',
+             'Pause Break', 'Back Space', 'Delete', 'Space', 'Enter', ])
+        for label in trivial_keys:
+            keymap[label] = gen_gen_simple_key_handler(label.lower().replace(' ', ''))
+        simple_keys = [
+            (u'\u2190', 'left'),
+            (u'\u2191', 'up'),
+            (u'\u2192', 'right'),
+            (u'\u2193', 'down'),
+        ]
+        for label, name in simple_keys:
+            keymap[label] = gen_gen_simple_key_handler(name)
+        shiftable_keys = ['! 1', '@ 2', '# 3', '$ 4', '% 5', '^ 6', '& 7', '* 8', '( 9', ') 0', '{ [', '} ]', '? /',
+                          '+ =',
+                          '_ -', '| \\', ]
+        for label in shiftable_keys:
+            keymap[label] = gen_gen_simple_key_handler(label[-1])
+        base_keymap = dict(keymap)
+        for label in shiftable_keys:
+            keymap[label] = gen_gen_simple_key_handler(label[0])
+        shifted_keymap = dict(keymap)
+        return {
+            'base': base_keymap,
+            'shifted': shifted_keymap,
+        }
+
+    def process_contacts(self):
+        contacts = self.morph.get_contacts()
+        for id, contact in contacts.items():
+            for key in self.layout.items():
+                key(shapely.geometry.Point(contact.x_pos, contact.y_pos))
+
+
 if __name__ == "__main__":
     morphs = open_all_morphs()
     assert morphs
+    assert len(morphs) == 2
+    keyboards = [
+        Keyboard(morph, layout_file)
+        for morph, layout_file in zip(morphs, ['morph-dvorak-left.svg', 'morph-dvorak-right.svg'])
+    ]
+
     try:
         while True:
-            for morph in morphs:
-                print_frame(morph.get_frame())
+            for keyboard in keyboards:
+                keyboard.process_contacts()
     except BaseException as e:
-        print('caught {} at {}. Exiting.'.format(e, traceback.print_tb(e.__traceback__)))
+        logging.error('caught {} at {}. Exiting.'.format(e, traceback.print_tb(e.__traceback__)))
         for morph in morphs:
             morph.close_sensel()
